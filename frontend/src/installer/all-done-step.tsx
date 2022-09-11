63e31e973e2bb5ab1984893775d41c26a853f3a0
Gallay Lajos <gallay.lajos@gmail.com>
2022-09-11 21:10:05 +0200

    updated all done step

--- frontend/src/installer/all-done-step.tsx
+++ frontend/src/installer/all-done-step.tsx
@@ -9,8 +9,9 @@ export const AllDoneStep = Shade<WizardStepProps>({
   render: ({ props }) => {
     return (
       <WizardStep title="Welcome to PI-RAT Installer" {...props}>
-        <lottie-player src={done} />
-        All done!
+        <lottie-player src={done} style={{ height: '128px' }} autoplay />
+        <h3>All done!</h3>
+        <p>Please finish the setup and log in with your fancy new admin account!</p>
       </WizardStep>
     )
   },
