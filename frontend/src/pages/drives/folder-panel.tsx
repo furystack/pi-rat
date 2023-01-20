9010ffba7656de7141ab9150ab7fef545350b683
Gallay Lajos <gallay.lajos@gmail.com>
2023-01-20 22:03:24 +0100

    progress on file browsing

--- frontend/src/pages/drives/folder-panel.tsx
+++ frontend/src/pages/drives/folder-panel.tsx
@@ -1,47 +1,58 @@
 import { createComponent, Shade } from '@furystack/shades'
-import type { CollectionService } from '@furystack/shades-common-components'
 import { Paper } from '@furystack/shades-common-components'
-import { PathHelper } from '@furystack/utils'
+import { ObservableValue, PathHelper } from '@furystack/utils'
 import type { Drive } from 'common'
 import { DriveSelector } from './drive-selector'
 import { FileList } from './file-list'
 
-export const FolderPanel = Shade<{ service: CollectionService<Drive> }>({
+export const FolderPanel = Shade<{ currentDrive: ObservableValue<Drive>; availableDrives: ObservableValue<Drive[]> }>({
   shadowDomName: 'folder-panel',
-  render: ({ props, useState, useObservable }) => {
-    const { service } = props
-    const [currentLetter, setCurrentLetter] = useState<string | undefined>('currentLetter', undefined)
-    const [currentPath, setCurrentPath] = useState('currentPath', '/')
-    useObservable('currentLetterObservable', service.focusedEntry, (entry) => {
-      setCurrentLetter(entry?.letter)
-      setCurrentPath('/')
-    })
+  render: ({ props, useObservable, element, useDisposable }) => {
+    const currentLetter = useDisposable(
+      'currentLetter',
+      () => new ObservableValue(props.currentDrive.getValue().letter),
+    )
+    const currentPath = useDisposable('currentPath', () => new ObservableValue('/'))
+
+    useObservable(
+      'currentLetterObservable',
+      props.currentDrive,
+      (entry) => {
+        entry && currentLetter.setValue(entry.letter)
+        currentPath.setValue('/')
+      },
+      true,
+    )
+    element.style.height = '100%'
+    element.style.width = '100%'
     return (
       <Paper
         elevation={1}
         style={{
-          width: '100%',
-          height: '100%',
           display: 'flex',
           flexDirection: 'column',
           flexGrow: '0',
           flexShrink: '0',
+          height: 'calc(100% - 42px)',
         }}>
-        <DriveSelector currentDrive={service.focusedEntry} />
+        <DriveSelector currentDrive={props.currentDrive} availableDrives={props.availableDrives} />
         <FileList
           currentDriveLetter={currentLetter}
           currentPath={currentPath}
           onActivate={(v) => {
             if (v.isDirectory) {
+              const path = currentPath.getValue()
               const newPath =
                 v.name === '..'
-                  ? currentPath && PathHelper.getSegments(currentPath).length > 1
-                    ? PathHelper.getParentPath(currentPath)
+                  ? path && PathHelper.getSegments(path).length > 1
+                    ? PathHelper.getParentPath(path)
                     : '/'
-                  : PathHelper.joinPaths(currentPath || '/', v.name)
-              setCurrentPath(newPath)
+                  : PathHelper.joinPaths(path || '/', v.name)
+              currentPath.setValue(newPath)
+              console.log('Changing path', { newPath })
+            } else {
+              console.log('onActivate', v)
             }
-            console.log('onActivate', v)
           }}
         />
       </Paper>
--- frontend/src/pages/drives/index.tsx
+++ frontend/src/pages/drives/index.tsx
@@ -1,6 +1,6 @@
-import { createComponent, Shade } from '@furystack/shades'
-import { CollectionService } from '@furystack/shades-common-components'
-import type { Drive } from 'common'
+import { createComponent, LazyLoad, Shade } from '@furystack/shades'
+import { Loader } from '@furystack/shades-common-components'
+import { ObservableValue } from '@furystack/utils'
 import { DrivesApiClient } from '../../services/drives-api-client'
 import { CreateDriveWizard } from './create-drive-wizard'
 import { FolderPanel } from './folder-panel'
@@ -8,62 +8,72 @@ import { FolderPanel } from './folder-panel'
 export const DrivesPage = Shade({
   shadowDomName: 'drives-page',
   render: ({ useDisposable, injector }) => {
-    const leftDrivesCollectionService = useDisposable(
-      'leftService',
-      () =>
-        new CollectionService<Drive>({
-          loader: async (options) => {
-            const data = await injector.getInstance(DrivesApiClient).call({
-              method: 'GET',
-              action: '/volumes',
-              query: {
-                findOptions: options,
-              },
-            })
-            return data.result
-          },
-          defaultSettings: {},
-        }),
-    )
-    const rightDrivesCollectionService = useDisposable(
-      'rightService',
-      () =>
-        new CollectionService<Drive>({
-          loader: async (options) => {
-            const data = await injector.getInstance(DrivesApiClient).call({
-              method: 'GET',
-              action: '/volumes',
-              query: {
-                findOptions: options,
-              },
-            })
-            return data.result
-          },
-          defaultSettings: {},
-        }),
-    )
+    const loadDrives = async () =>
+      await injector.getInstance(DrivesApiClient).call({
+        method: 'GET',
+        action: '/volumes',
+        query: {
+          findOptions: {},
+        },
+      })
+
     return (
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          top: '48px',
-          position: 'fixed',
-          flexDirection: 'row',
-          gap: '8px',
-          height: 'calc(100% - 48px)',
-          width: '100%',
-        }}>
-        <FolderPanel service={leftDrivesCollectionService} />
-        <FolderPanel service={rightDrivesCollectionService} />
-        <CreateDriveWizard
-          onDriveAdded={() => {
-            leftDrivesCollectionService.getEntries({ ...leftDrivesCollectionService.querySettings.getValue() })
-            rightDrivesCollectionService.getEntries({ ...rightDrivesCollectionService.querySettings.getValue() })
-          }}
-        />
-      </div>
+      <LazyLoad
+        loader={<Loader />}
+        component={async () => {
+          const drives = await loadDrives()
+
+          const availableDrives = useDisposable('availableDrives', () => new ObservableValue(drives.result.entries))
+          availableDrives.setValue(drives.result.entries)
+          if (!drives.result.entries.length) {
+            return (
+              <>
+                <div>No drive created yet.</div>
+                <CreateDriveWizard
+                  onDriveAdded={async () => {
+                    const reloadedDrives = await loadDrives()
+                    availableDrives.setValue(reloadedDrives.result.entries)
+                  }}
+                />
+              </>
+            )
+          }
+
+          const currentLeftDrive = useDisposable(
+            'currentLeftDrive',
+            () => new ObservableValue(drives.result.entries[0]),
+          )
+
+          const currentRightDrive = useDisposable(
+            'currentRightDrive',
+            () => new ObservableValue(drives.result.entries[0]),
+          )
+
+          return (
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                top: '56px',
+                position: 'fixed',
+                flexDirection: 'row',
+                gap: '8px',
+                height: 'calc(100% - 48px)',
+                width: '100%',
+              }}>
+              <FolderPanel currentDrive={currentLeftDrive} availableDrives={availableDrives} />
+              <FolderPanel currentDrive={currentRightDrive} availableDrives={availableDrives} />
+              <CreateDriveWizard
+                onDriveAdded={async () => {
+                  const reloadedDrives = await loadDrives()
+                  availableDrives.setValue(reloadedDrives.result.entries)
+                }}
+              />
+            </div>
+          )
+        }}
+      />
     )
   },
 })
