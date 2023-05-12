30d87600f70914ecfd755ab9aa33f26a852ccb25
Gallay Lajos <gallay.lajos@gmail.com>
2023-05-12 17:32:04 +0200

    fixed worker config

--- frontend/src/components/worker-config.ts
+++ frontend/src/components/worker-config.ts
@@ -1,8 +1,8 @@
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker&inline'
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker&inline'
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker&inline'
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker&inline'
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker&inline'
 
 self.MonacoEnvironment = {
   getWorker(_, label) {
