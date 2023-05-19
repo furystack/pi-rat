2f42de83be1388b0ac2976a8379a4ceb356ac8cb
Gallay Lajos <gallay.lajos@gmail.com>
2023-05-19 16:20:04 +0200

    continue watching command

--- frontend/src/components/command-palette/index.tsx
+++ frontend/src/components/command-palette/index.tsx
@@ -2,10 +2,16 @@ import { Shade, createComponent } from '@furystack/shades'
 import { CommandPalette } from '@furystack/shades-common-components'
 import { browserCommandProvider } from './command-providers/browser.js'
 import { entitiesCommandProvider } from './command-providers/entities.js'
+import { continueWatchingCommandProvider } from './command-providers/continue-watching.js'
 
 export const PiRatCommandPalette = Shade({
   shadowDomName: 'pirat-command-palette',
   render: () => {
-    return <CommandPalette commandProviders={[browserCommandProvider, entitiesCommandProvider]} defaultPrefix=">" />
+    return (
+      <CommandPalette
+        commandProviders={[browserCommandProvider, entitiesCommandProvider, continueWatchingCommandProvider]}
+        defaultPrefix=">"
+      />
+    )
   },
 })
