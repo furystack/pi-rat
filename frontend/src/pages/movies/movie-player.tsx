03779a190eafb1b5d361286144873fa6feb0e859
Gallay Lajos <gallay.lajos@gmail.com>
2023-12-18 16:20:59 +0100

    chore: Movie player improvements, subtitle fixes

--- frontend/src/pages/movies/movie-player.tsx
+++ frontend/src/pages/movies/movie-player.tsx
@@ -1,104 +1,92 @@
-import { createComponent, Shade } from '@furystack/shades'
+import { attachProps, createComponent, Shade } from '@furystack/shades'
 import { environmentOptions } from '../../environment-options.js'
 import 'video.js'
 import 'video.js/dist/video-js.css'
-import videojsDefault from 'video.js'
+import * as videojsDefault from 'video.js'
 import { WatchProgressUpdater } from '../../services/watch-progress-updater.js'
 import { WatchProgressService } from '../../services/watch-progress-service.js'
-import { MovieFilesService } from '../../services/movie-files-service.js'
-import { PiRatLazyLoad } from '../../components/pirat-lazy-load.js'
+import type { MovieFile, MovieWatchHistoryEntry } from 'common'
 
-const videojs = videojsDefault as any as typeof videojsDefault.default & any
+const videojs = videojsDefault as any as typeof videojsDefault.default /* & any*/
 
-export const MoviePlayer = Shade<{ movieFileId: string }>({
+interface MoviePlayerProps {
+  movieFile: MovieFile
+  watchProgress?: MovieWatchHistoryEntry
+}
+
+export const MoviePlayer = Shade<MoviePlayerProps>({
   shadowDomName: 'pirat-movie-player',
-  render: ({ props, element, injector, useDisposable }) => {
-    const movieFilesService = injector.getInstance(MovieFilesService)
+  constructed: ({ props, element, injector, useDisposable }) => {
+    const { movieFile } = props
+    const video = element.querySelector('video') as any as HTMLVideoElement
+
+    const player = videojs.default(video, {
+      controls: true,
+      autoplay: true,
+      html5: {
+        nativeCaptions: false,
+      },
+    })
+
+    const { fileName, path } = movieFile
     const watchProgressService = injector.getInstance(WatchProgressService)
-    const { movieFileId } = props
+
+    useDisposable(
+      'watchProgressUpdater',
+      () =>
+        new WatchProgressUpdater({
+          intervalMs: 10 * 1000,
+          onSave: async (progress) => {
+            watchProgressService.updateWatchEntry({
+              completed: video.duration - progress < 10,
+              driveLetter: movieFile.driveLetter,
+              path,
+              fileName,
+              imdbId: movieFile.imdbId,
+              watchedSeconds: progress,
+            })
+          },
+          saveTresholdSeconds: 10,
+          videoElement: video,
+        }),
+    )
+
+    attachProps(element, { player })
+
+    return () => player.dispose()
+  },
+  render: ({ props }) => {
+    const { movieFile, watchProgress } = props
+
+    const subtitleTracks = movieFile.ffprobe.streams
+      .filter((stream) => (stream.codec_type as any) === 'subtitle')
+      .map((subtitle) => (
+        <track
+          kind="captions"
+          label={subtitle.tags.title || subtitle.tags.language || subtitle.tags.filename}
+          src={`${environmentOptions.serviceUrl}/drives/files/${encodeURIComponent(
+            movieFile.driveLetter,
+          )}/${encodeURIComponent(`${movieFile.path}/${movieFile.fileName}-subtitle-${subtitle.index}.vtt`)}/download`}
+          srclang={subtitle.tags.language}
+        />
+      ))
 
     return (
-      <PiRatLazyLoad
-        component={async () => {
-          const movieFile = await movieFilesService.getMovieFile(movieFileId)
-          if (!movieFile) {
-            throw new Error(`Movie file with id ${movieFileId} not found`)
-          }
-          const { fileName, path } = movieFile
-          const {
-            entries: [watchProgress],
-          } = await watchProgressService.findWatchProgressForFile({
-            path,
-            driveLetter: movieFile.driveLetter,
-            fileName,
-          })
-
-          setTimeout(() => {
-            const video = element.querySelector('video') as HTMLVideoElement
-
-            const { ffprobe } = movieFile
-            const player = videojs(video)
-
-            ffprobe.streams
-              .filter((stream) => (stream.codec_type as any) === 'subtitle')
-              .forEach((subtitle) => {
-                player.addRemoteTextTrack({
-                  kind: 'subtitles',
-                  label: subtitle.tags.title || subtitle.tags.language || subtitle.tags.filename,
-                  src: `${environmentOptions.serviceUrl}/media/stream/${encodeURIComponent(
-                    movieFile.driveLetter,
-                  )}/${encodeURIComponent(`${movieFile.path}-subtitle-${subtitle.index}.vtt`)}/download`,
-                  srcLang: subtitle.tags.language,
-                })
-              })
-
-            useDisposable(
-              'watchProgressUpdater',
-              () =>
-                new WatchProgressUpdater({
-                  intervalMs: 10 * 1000,
-                  onSave: async (progress) => {
-                    watchProgressService.updateWatchEntry({
-                      completed: video.duration - progress < 10,
-                      driveLetter: movieFile.driveLetter,
-                      path,
-                      fileName,
-                      imdbId: movieFile.imdbId,
-                      watchedSeconds: progress,
-                    })
-                  },
-                  saveTresholdSeconds: 10,
-                  videoElement: video,
-                }),
-            )
-          }, 100)
-
-          return (
-            <video
-              style={{
-                width: '100%',
-                height: '100%',
-                position: 'fixed',
-              }}
-              className="video-js"
-              crossOrigin="use-credentials"
-              // data-setup={JSON.stringify({
-              //   controls: true,
-              //   autoplay: true,
-              //   preload: 'auto',
-              //   withCredentials: true,
-              // })}
-              autoplay
-              currentTime={watchProgress?.watchedSeconds || 0}
-              controls>
-              <source
-                src={`${environmentOptions.serviceUrl}/media/movie-files/${encodeURIComponent(movieFileId)}/stream`}
-                type="video/mp4"
-              />
-            </video>
-          )
+      <video
+        style={{
+          width: '100%',
+          height: '100%',
+          position: 'fixed',
         }}
-      />
+        className="video-js"
+        crossOrigin="use-credentials"
+        currentTime={watchProgress?.watchedSeconds || 0}>
+        <source
+          src={`${environmentOptions.serviceUrl}/media/movie-files/${encodeURIComponent(movieFile.id)}/stream`}
+          type="video/mp4"
+        />
+        {...subtitleTracks}
+      </video>
     )
   },
 })
