dadd6c675fa367a42e43bbe1723c3776b60bfae4
Gallay Lajos <gallay.lajos@gmail.com>
2024-05-09 12:34:56 +0200

    improved layout

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -22,40 +22,38 @@ export const Login = Shade({
         }
       },
     })
+    Object.assign(element.style, {
+      padding: '1em',
+      marginTop: '48px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    })
     return (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: 'calc(100% - 64px)',
-          padding: '0 100px',
-          paddingTop: '100px',
-        }}>
-        <Paper elevation={3}>
-          <Form<LoginPayload>
-            validate={(plainData: any): plainData is LoginPayload => {
-              return plainData?.userName?.length && plainData?.password?.length
-            }}
-            className="login-form"
-            onSubmit={({ userName, password }) => sessionService.login(userName, password)}>
-            <h2>Login</h2>
-            <Input labelTitle="E-mail address" name="userName" required autofocus type="email" />
-            <Input labelTitle="Password" name="password" required minLength={4} type="password" />
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                flexDirection: 'row',
-                padding: '1em 0',
-              }}>
-              <Button variant="contained" color="primary" type="submit">
-                Login
-              </Button>
-            </div>
-          </Form>
-        </Paper>
-      </div>
+      <Paper elevation={3} style={{ flexGrow: '1' }}>
+        <Form<LoginPayload>
+          validate={(plainData: any): plainData is LoginPayload => {
+            return plainData?.userName?.length && plainData?.password?.length
+          }}
+          className="login-form"
+          onSubmit={({ userName, password }) => sessionService.login(userName, password)}>
+          <h2>Login</h2>
+          <Input labelTitle="E-mail address" name="userName" required autofocus type="email" />
+          <Input labelTitle="Password" name="password" required minLength={4} type="password" />
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              flexDirection: 'row',
+              padding: '1em 0',
+            }}>
+            <Button variant="contained" color="primary" type="submit">
+              Login
+            </Button>
+          </div>
+        </Form>
+      </Paper>
     )
   },
 })
