128fd1e199361b62aba6b235d5b70f4d682d79ea
Gallay Lajos <gallay.lajos@gmail.com>
2024-06-03 17:13:42 +0200

    config updates

--- common/src/models/config/movies-config.ts
+++ common/src/models/config/movies-config.ts
@@ -2,8 +2,24 @@ export interface MoviesConfig {
   id: 'MOVIES_CONFIG'
   value: {
     /**
+     * Search .nfo files for imdb id
+     */
+    enableNfoParsing: boolean
+
+    /**
      * 'all' or a list of directories to watch for new movies.
      */
-    watchFiles: 'all' | Array<{ drive: string; path: string }>
+    watchFiles:
+      | 'all'
+      | Array<{
+          /**
+           * The drive to watch
+           */
+          drive: string
+          /**
+           * An optional path filter. If not set, all files in the drive will be watched.
+           */
+          path?: string
+        }>
   }
 }
