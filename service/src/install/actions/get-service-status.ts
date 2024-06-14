66671924d199830723dcf1e17f44fee692f03568
Lajos Gallay <lajos_gallay@trimble.com>
2024-06-14 11:53:24 +0200

    get rid of webtorrent

--- service/src/install/actions/get-service-status.ts
+++ service/src/install/actions/get-service-status.ts
@@ -3,19 +3,16 @@ import { JsonResult } from '@furystack/rest-service'
 import type { GetServiceStatusAction } from 'common'
 import { ServiceStatusProvider } from '../service-installer.js'
 import { OmdbClientService } from '../../media/metadata-services/omdb-client-service.js'
-import { TorrentClient } from '../../webtorrent/torrent-client.js'
 
 export const GetServiceStatus: RequestAction<GetServiceStatusAction> = async ({ injector }) => {
   const state = await injector.getInstance(ServiceStatusProvider).getStatus()
 
   const omdb = !!injector.getInstance(OmdbClientService).config
-  const torrent = !!injector.getInstance(TorrentClient).getConfig()
 
   return JsonResult({
     state,
     services: {
       omdb,
-      torrent,
       github: false,
     },
   })
