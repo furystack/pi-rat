c752e25bcb4ffef3ae13d1594456a1b067f51e56
Gallay Lajos <gallay.lajos@gmail.com>
2024-08-15 11:02:08 +0200

    lint cfg fix

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,7 @@ export default tseslint.config(
     },
     languageOptions: {
       parserOptions: {
-        project: ['tsconfig.json'],
+        project: ['tsconfig.eslint.json'],
         tsconfigRootDir: import.meta.dirname,
       },
     },
