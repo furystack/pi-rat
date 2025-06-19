5c671de7d12c92caff3e37c217b832b7b5f67843
Lajos <gallayl@users.noreply.github.com>
2025-06-19 11:24:53 +0200

    chore: dependency updates (#82)

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,26 @@ export default defineConfig({
       enabled: true,
       include: ['common/src/**/*.ts', 'frontend/src/**/*.ts', 'service/src/**/*.ts'],
     },
+    projects: [
+      {
+        test: {
+          name: 'Common',
+          include: ['common/src/**/*.spec.ts'],
+        },
+      },
+      {
+        test: {
+          name: 'Service',
+          include: ['service/src/**/*.spec.ts'],
+        },
+      },
+      {
+        test: {
+          name: 'Frontend',
+          environment: 'jsdom',
+          include: ['frontend/src/**/*.spec.(ts|tsx)'],
+        },
+      },
+    ],
   },
 })
