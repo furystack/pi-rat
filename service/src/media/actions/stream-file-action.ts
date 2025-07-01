af072f5d6071babf2d65c40aeb4bfb4d5b4a75f5
Gallay Lajos <gallay.lajos@gmail.com>
2025-07-01 16:33:39 +0200

    added ffmpeg args to cache

--- service/src/media/actions/stream-file-action.ts
+++ service/src/media/actions/stream-file-action.ts
@@ -1,12 +1,9 @@
 import { getLogger } from '@furystack/logging'
-import { RequestError } from '@furystack/rest'
 import type { RequestAction } from '@furystack/rest-service'
 import { BypassResult } from '@furystack/rest-service'
 import { spawn } from 'child_process'
 import type { StreamFileEndpoint } from 'common'
 import mime from 'mime'
-import { join } from 'path'
-import { FfprobeService } from '../../ffprobe-service.js'
 import { StreamFileActionCaches } from '../services/stream-file-action-caches.js'
 
 export const StreamAction: RequestAction<StreamFileEndpoint> = async ({
@@ -16,19 +13,6 @@ export const StreamAction: RequestAction<StreamFileEndpoint> = async ({
   getQuery,
 }) => {
   const logger = getLogger(injector).withScope('StreamAction')
-
-  const { letter, path } = getUrlParams()
-  const { from, to, audio, video } = getQuery()
-
-  const caches = injector.getInstance(StreamFileActionCaches)
-
-  const drive = await caches.driveCache.get(letter)
-
-  if (!drive) {
-    throw new RequestError(`Drive ${letter} not found`, 404)
-  }
-
-  const fullPath = join(drive.physicalPath, path)
   const mimeType = mime.getType('mp4')
   const mimeHeader = mimeType ? { 'Content-Type': mimeType } : {}
   const head = {
@@ -36,83 +20,22 @@ export const StreamAction: RequestAction<StreamFileEndpoint> = async ({
   }
   response.writeHead(200, head)
 
-  const ffprobe = await injector.getInstance(FfprobeService).getFfprobeForPiratFile({ driveLetter: letter, path })
+  const { letter, path } = getUrlParams()
 
-  const audioStreams = ffprobe.streams.filter((stream) => stream.codec_type === 'audio')
-  const audioStream = audioStreams.find((track) => track.index === audio?.trackId) || audioStreams[0]
-  const videoStream = ffprobe.streams.find((stream) => stream.codec_type === 'video')
+  const cache = injector.getInstance(StreamFileActionCaches)
 
-  await logger.information({
-    message: `Starting stream from ${from} to ${to}, transcodeVideo: ${!!video?.codec}, transcodeAudio: ${!!audio?.audioCodec}`,
-    data: { fullPath, from, to, audio, video, audioStream, videoStream },
+  const ffmpegArgs = await cache.ffMpegArgsCache.get({
+    file: {
+      driveLetter: letter,
+      path,
+    },
+    queryParams: getQuery(),
+    injector,
   })
 
-  // Build ffmpeg args
-  const ffmpegArgs: string[] = []
-
   // Fast input seeking for better performance
-  if (typeof from === 'number') {
-    ffmpegArgs.push('-ss', String(from))
-  }
-
-  ffmpegArgs.push('-i', fullPath, '-f', 'mp4', '-movflags', 'empty_moov+frag_keyframe+faststart+default_base_moof')
-
-  if (typeof to === 'number' && typeof from === 'number') {
-    ffmpegArgs.push('-t', String(Math.max(to - from, 1)))
-  }
-
-  const audioStreamIndex = Math.max(
-    0,
-    audioStreams.findIndex((stream) => stream === audioStream),
-  )
-  ffmpegArgs.push('-map', `0:a:${audioStreamIndex}`)
-
-  if (audio?.audioCodec) {
-    ffmpegArgs.push('-c:a', audio.audioCodec)
-  } else {
-    ffmpegArgs.push('-c:a', 'aac')
-    ffmpegArgs.push('-b:a', '128k')
-  }
-  if (audio?.bitrate) {
-    ffmpegArgs.push('-b:a', String(audio.bitrate))
-  }
-  if (audio?.mixdown) {
-    ffmpegArgs.push('-ac', '2')
-  }
-
-  const videoStreamIndex = 0
-  ffmpegArgs.push('-map', `0:v:${videoStreamIndex}`)
-  if (video?.codec) {
-    ffmpegArgs.push('-c:v', video.codec)
-  } else {
-    ffmpegArgs.push('-c:v', 'libx264')
-    ffmpegArgs.push('-preset', 'ultrafast') // Use a fast preset for lower latency
-  }
-  if (video?.resolution) {
-    switch (video.resolution) {
-      case '4k':
-        ffmpegArgs.push('-s', '3840x2160')
-        break
-      case '1080p':
-        ffmpegArgs.push('-s', '1920x1080')
-        break
-      case '720p':
-        ffmpegArgs.push('-s', '1280x720')
-        break
-      case '480p':
-        ffmpegArgs.push('-s', '854x480')
-        break
-      case '360p':
-        ffmpegArgs.push('-s', '640x360')
-        break
-      default:
-        break
-    }
-  }
 
   const abortController = new AbortController()
-
-  ffmpegArgs.push('pipe:1') // Output to stdout
 
   await logger.verbose({ message: `Spawning ffmpeg with args: ${ffmpegArgs.join(' ')}` })
 
--- service/src/media/services/stream-file-action-caches.ts
+++ service/src/media/services/stream-file-action-caches.ts
@@ -1,7 +1,9 @@
 import { Cache } from '@furystack/cache'
 import { getStoreManager } from '@furystack/core'
 import { Injectable, type Injector } from '@furystack/inject'
-import { Drive } from 'common'
+import { Drive, type PiRatFile, type StreamQueryParams } from 'common'
+import { join } from 'path'
+import { FfprobeService } from '../../ffprobe-service.js'
 
 @Injectable({
   lifetime: 'singleton',
@@ -14,6 +16,100 @@ export class StreamFileActionCaches {
       const driveStore = getStoreManager(this.injector).getStoreFor(Drive, 'letter')
       const drive = await driveStore.get(key)
       return drive
+    },
+  })
+
+  public ffMpegArgsCache = new Cache({
+    capacity: 100,
+    load: async ({
+      injector,
+      queryParams,
+      file,
+    }: {
+      injector: Injector
+      queryParams: StreamQueryParams
+      file: PiRatFile
+    }) => {
+      const { audio, video, from, to } = queryParams
+
+      const [drive, ffprobe] = await Promise.all([
+        this.driveCache.get(file.driveLetter),
+        injector.getInstance(FfprobeService).getFfprobeForPiratFile(file),
+      ])
+
+      if (!drive) {
+        throw new Error(`Drive ${file.driveLetter} not found`)
+      }
+
+      const fullPath = join(drive.physicalPath, file.path)
+
+      const audioStreams = ffprobe.streams.filter((stream) => stream.codec_type === 'audio')
+      const audioStream = audioStreams.find((track) => track.index === audio?.trackId) || audioStreams[0]
+
+      // Build ffmpeg args
+      const ffmpegArgs: string[] = []
+
+      if (typeof from === 'number') {
+        ffmpegArgs.push('-ss', String(from))
+      }
+
+      ffmpegArgs.push('-i', fullPath, '-f', 'mp4', '-movflags', 'empty_moov+frag_keyframe+faststart+default_base_moof')
+
+      if (typeof to === 'number' && typeof from === 'number') {
+        ffmpegArgs.push('-t', String(Math.max(to - from, 1)))
+      }
+
+      const audioStreamIndex = Math.max(
+        0,
+        audioStreams.findIndex((stream) => stream === audioStream),
+      )
+      ffmpegArgs.push('-map', `0:a:${audioStreamIndex}`)
+
+      if (audio?.audioCodec) {
+        ffmpegArgs.push('-c:a', audio.audioCodec)
+      } else {
+        ffmpegArgs.push('-c:a', 'aac')
+        ffmpegArgs.push('-b:a', '128k')
+      }
+      if (audio?.bitrate) {
+        ffmpegArgs.push('-b:a', String(audio.bitrate))
+      }
+      if (audio?.mixdown) {
+        ffmpegArgs.push('-ac', '2')
+      }
+
+      const videoStreamIndex = 0
+      ffmpegArgs.push('-map', `0:v:${videoStreamIndex}`)
+      if (video?.codec) {
+        ffmpegArgs.push('-c:v', video.codec)
+      } else {
+        ffmpegArgs.push('-c:v', 'libx264')
+        ffmpegArgs.push('-preset', 'ultrafast') // Use a fast preset for lower latency
+      }
+      if (video?.resolution) {
+        switch (video.resolution) {
+          case '4k':
+            ffmpegArgs.push('-s', '3840x2160')
+            break
+          case '1080p':
+            ffmpegArgs.push('-s', '1920x1080')
+            break
+          case '720p':
+            ffmpegArgs.push('-s', '1280x720')
+            break
+          case '480p':
+            ffmpegArgs.push('-s', '854x480')
+            break
+          case '360p':
+            ffmpegArgs.push('-s', '640x360')
+            break
+          default:
+            break
+        }
+      }
+      ffmpegArgs.push('pipe:1') // Output to stdout
+
+      return ffmpegArgs
     },
   })
 }
