71102b4651b52fd2b609688324468c134c66ae76
Gallay Lajos <gallay.lajos@gmail.com>
2026-01-25 16:08:25 +0100

    try fix e2e

--- e2e/helpers.ts
+++ e2e/helpers.ts
@@ -5,6 +5,7 @@ import { basename } from 'path'
 
 export const assertAndDismissNoty = async (page: Page, text: string) => {
   const noty = page.locator('shade-noty', { hasText: text })
+  await noty.waitFor({ state: 'visible' })
   const closeNoty = noty.locator('button.dismissNoty')
   await closeNoty.click()
   await noty.waitFor({ state: 'detached' })
